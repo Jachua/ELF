b747e30f6d4587750c353d5d127188ef2d100635
XINDONG <jacquelinemxh@yahoo.com>
2018-08-02 10:24:26 +0800

    set difficulty level

--- scripts/elfgames/go/gtp.sh
+++ scripts/elfgames/go/gtp.sh
@@ -6,8 +6,14 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-MODEL=$1
 shift
+
+case $1 in 
+EASY) LEVEL="4";;
+MEDIUM) LEVEL="256";;
+HARD) LEVEL="16384";;
+*) LEVEL="256";;
+esac
 
 game=elfgames.go.game model=df_pred model_file=elfgames.go.df_model3 python3 df_console.py --mode online --keys_in_reply V rv \
     --use_mcts --mcts_verbose_time --mcts_use_prior --mcts_persistent_tree \
@@ -17,5 +23,5 @@ game=elfgames.go.game model=df_pred model_file=elfgames.go.df_model3 python3 df_
     --no_check_loaded_options \
     --no_parameter_print \
     --verbose --gpu 0 --num_block 20 --dim 224 --mcts_puct 1.50 --batchsize 16 \
-    --mcts_rollout_per_batch 16 --mcts_threads 2 --mcts_rollout_per_thread 64 \
+    --mcts_rollout_per_batch 16 --mcts_threads 2 --mcts_rollout_per_thread $LEVEL \
     --resign_thres 0.05 --mcts_virtual_loss 1
